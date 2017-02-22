fcfe1c657f13caf3b47b118e338b843bf4590341
aormaetxea <anderormaetxea@gmail.com>
2017-02-22 22:26:13 +0100

    Se termina el tutorial de refactorización

--- index.js
+++ index.js
@@ -1,35 +1,16 @@
 'use strict'
 
-const express = require('express')
-const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const app = require('./app')
+const config = require('./config')
 
-const app = express()
-const port = process.env.PORT || 3001
+mongoose.connect(config.db, (err,res) => {
+  if(err) {
+    return console.log(`Error al conectar a la base de datos: ${err}`)
+  }
+  console.log('Conectado a base de datos')
 
-app.use(bodyParser.urlencoded({ extended: false}))
-app.use(bodyParser.json())
-
-app.get('/api/product', (req,res) => {
-  res.status(200).send({ products: []})
-})
-
-app.get('/api/product/:productId', (req,res) => {
-
-})
-
-app.post('/api/product', (req,res) => {
-  console.log(req.body)
-  res.status(200).send({message: 'El producto se ha recibido'})
-})
-
-app.put('/api/product/:productId', (req,res) => {
-
-})
-
-app.delete('/api/product/:productId', (req,res) => {
-
-})
-
-app.listen(port, () => {
-  console.log(`API corriendo ${port}`)
+  app.listen(config.port, () => {
+    console.log(`API corriendo ${config.port}`)
+  })
 })
